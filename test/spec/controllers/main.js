84f22ecf8ea4fd7cfe9218822171f7b1c9a47b70
Walter Miller <walter@waltermiller.ca>
2013-07-03 17:23:57 -0400

    Pushing a broken test to test jenkins

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -19,4 +19,8 @@ describe('Controller: MainCtrl', function () {
   it('should attach a list of awesomeThings to the scope', function () {
     expect(scope.awesomeThings.length).toBe(3);
   });
+
+  it('should fail to test jenkins', function () {
+    expect(4).toBe(3);
+  });
 });
